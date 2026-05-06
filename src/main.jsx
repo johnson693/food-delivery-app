660719abc296fc7e9262df484f03bec5278303c8
johnson693 <johnsonbalnad2512@gmail.com>
2026-05-06 12:28:26 +0530

    Initial commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'   // 👈 change here
 import StoreContextProvider from './context/StoreContext.jsx'
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
-    <StoreContextProvider>
-      <App />
+    <HashRouter>   {/* 👈 change here */}
+      <StoreContextProvider>
+        <App />
       </StoreContextProvider>
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>
 )
